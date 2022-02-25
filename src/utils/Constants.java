00f59ba230d79d87e4db8b042d234eb4b5967f4d
Remakelo <tio-own96@hotmail.com>
2022-02-25 12:18:23 +0800

    testing?

---
+++ src/utils/Constants.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Constants {
+    
+    public static InetAddress serverIP;
+    static {
+        try {
+            serverIP = InetAddress.getByName(null);
+         }
+            catch (UnknownHostException ex) {
+
+            }
+        }
+    public static final int serverPortNumber = 6666;
+    public static final int messageLength = 65535;
+    public static int clientPort = 123;
+}
