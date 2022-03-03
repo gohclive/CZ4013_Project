717b1b21d3343f7a494ef9e987b6e0ed1f775588
clive goh <goh.clive@gmail.com>
2022-03-04 04:08:35 +0800

    merged constants file

--- src/entity/Constants.java
+++ src/entity/Constants.java
@@ -1,9 +1,31 @@
 package entity;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class Constants {
-	
-	public enum CURRENCY{
-		SGD,USD,HKD,EURO,JPY,GBP
+
+	public enum CURRENCY {
+		SGD, USD, HKD, EURO, JPY, GBP
+	}
+
+	public static InetAddress serverIP;
+	static {
+		try {
+			serverIP = InetAddress.getByName(null);
+		} catch (UnknownHostException ex) {
+
+		}
+	}
+	public static final int serverPortNumber = 6666;
+	public static final int messageLength = 65535;
+	public static int clientPort = 122;
+	public static final Charset commonCharset = StandardCharsets.UTF_8;
+
+	public static void main(String[] args) {
+		System.out.println(serverIP);
 	}
 
 }
