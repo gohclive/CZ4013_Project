fb52b40ec2daab6dba716ec67924f5ddf9b667b7
clive goh <goh.clive@gmail.com>
2022-03-14 06:40:53 +0800

    added errors message as constants

--- src/entity/Constants.java
+++ src/entity/Constants.java
@@ -5,11 +5,15 @@ import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
+import utils.Marshal;
+
 public class Constants {
 
 	public enum CURRENCY {
 		SGD, USD, HKD, EURO, JPY, GBP
 	}
+	
+	public static final int PACKETLOSTPROB = 0;
 
 	public static InetAddress serverIP;
 	static {
@@ -27,9 +31,24 @@ public class Constants {
 	public static int clientPort = 122;
 	public static final Charset commonCharset = StandardCharsets.UTF_8;
 	public static int messageIdentifer = 0;
+	
+	
+	//error messages
+	public static final String ACCOUNTNOTFOUND = "|ERROR|"+ "Account not found!" + "|";
+	public static final String INCORRECTNAME = "|ERROR|"+ "Account name incorrect!" + "|";
+	public static final String INCORRECTPASSWORD = "|ERROR|"+ "Account password incorrect!" + "|";
+	public static final String INSUFFICIENTBAL ="|ERROR|"+ "Account has insufficent balance!" + "|";
+	
 
 	public static void main(String[] args) {
 		System.out.println(serverIP);
+		System.out.println(CURRENCY.valueOf("USD"));
+		System.out.println(CURRENCY.valueOf("SGD"));
+		String msg = "1|clivegoh|12345678|SGD";
+		String[] decoded = Marshal.decodeForServer(msg);
+		System.out.println(decoded[3]);
+		System.out.println(CURRENCY.values()[0]);
 	}
+	
 
 }
