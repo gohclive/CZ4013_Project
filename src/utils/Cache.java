68530d4c6296c070d77bbdca46eb15dcbe7845a6
Remakelo <tio-own96@hotmail.com>
2022-03-29 14:15:03 +0800

    .

--- src/utils/Cache.java
+++ src/utils/Cache.java
@@ -1,5 +1,12 @@
 package utils;
+import java.net.DatagramPacket;
+import java.util.HashMap;
+import utils.Marshal;
+import entity.Constants;
+import utils.Connections;
 
 public class Cache {
-    
+    public void addToCache(DatagramPacket receivedmsg){
+        
+    }
 }
