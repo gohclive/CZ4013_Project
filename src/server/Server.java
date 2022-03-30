2b1772e5bda12f905bb8abe66e04f376ef2cb179
clive goh <goh.clive@gmail.com>
2022-03-31 00:03:06 +0800

    added checks

--- src/server/Server.java
+++ src/server/Server.java
@@ -77,8 +77,9 @@ public class Server {
 			DatagramPacket DpReceive = null; // make packet to receive datagrampacket
 			Queue<DatagramPacket> receiveQueue = new LinkedList<>();
 			while (true) {
-				if(getRand() >= Constants.PACKETLOSTPROB){
-					DpReceive = new DatagramPacket(receive, receive.length); // packe to read buffer and length of buffer
+				if (getRand() >= Constants.PACKETLOSTPROB) {
+					DpReceive = new DatagramPacket(receive, receive.length); // packe to read buffer and length of
+																				// buffer
 					ds.receive(DpReceive); // to receive the msg
 					receiveQueue.add(DpReceive); // add to queue
 					if (!receiveQueue.isEmpty()) {
@@ -89,195 +90,199 @@ public class Server {
 							// System.out.println("Queue is empty!");
 						}
 					}
-				}
-				else{
+				} else {
 					System.out.println("SIMULATED PACKET LOSS, SERVER IGNORE PACKET");
 				}
-				
-				String decodedMsg = Marshal.byteToString(receive);
-				String[] idcontent = Marshal.decodeForServer(decodedMsg);
-				String[] message = Marshal.decodeMessage(idcontent[1]);
-				String msg = "";
-				Account a = null;
-				if (msgCache != null) {
-					if (msgCache.containsKey(Integer.parseInt(idcontent[0]))) {
-						System.out.println("Duplicated Message ID: " + idcontent[0] + " is Received!");
-						String result = resultcache.get(Integer.parseInt(idcontent[0]));
-						msg = result;
-						System.out.println("Sending Following Message to Client: " + msg);
-						Connections.sendMsgToClient(msg, DpReceive);
-						continue;
-					} else {
-						msgCache.put(Integer.parseInt(idcontent[0]), idcontent[1]);
-						System.out.println("I Have inputted id content 0 " + idcontent[0] + " and idcontent 1 " + idcontent[1]);
-					}
-					
-				}
-				switch (Integer.parseInt(message[0])) {
-					case 0:
-						break;
-					case 1:
-						// create account
-						a = CreateAccount(accountnum, message[1], message[2], CURRENCY.valueOf(message[3]));
-						accList.add(a);
-						accountnum++;
-						msg = "1|SUCCESS|" + "Account successfully created, your account number is "
-								+ Marshal.accPadding(a.getNumber());
-						resultcache.put(Integer.parseInt(idcontent[0]), msg);
-						Connections.sendMsgToClient(msg, DpReceive);
-						break;
-					case 2:
-						// close account
-						a = checkAccount(accList, message[2]);
-						if (a == null) {
-							msg = "2" + Constants.ACCOUNTNOTFOUND;
-						} else if (!a.getName().equals(message[1])) {
-							msg = "2" + Constants.INCORRECTNAME;
-						} else if (!a.getPassword().equals(message[3])) {
-							msg = "2" + Constants.INCORRECTPASSWORD;
-						} else {
-							if (a.getBalance() >= 1) {
-								msg = "2|ERROR|"
-										+ "Account still have balance left! Please withdraw all amount before closing your account"
-										+ "|";
-							} else {
-								accList.remove(a);
-								msg = "2|SUCCESS|" + Marshal.accPadding(a.getNumber()) + " has been closed!|";
-							}
-						}
-						resultcache.put(Integer.parseInt(idcontent[0]), msg);
-						Connections.sendMsgToClient(msg, DpReceive);
-						break;
-					case 3:
-						// deposit account
-						a = checkAccount(accList, message[2]);
-						if (a == null) {
-							msg = "3" + Constants.ACCOUNTNOTFOUND;
-						} else if (!a.getName().equals(message[1])) {
-							msg = "3" + Constants.INCORRECTNAME;
-						} else if (!a.getPassword().equals(message[3])) {
-							msg = "3" + Constants.INCORRECTPASSWORD;
-						} else {
-							// TODO currency conversion
-							int i = accList.indexOf(a);
-							double bal = accList.get(i).getBalance();
-							accList.get(i).setBalance(bal + Double.parseDouble(message[5]));
-							msg = "3|SUCCESS|" + a.getNumber() + " balance is " + accList.get(i).getBalance() + "|";
-						}
-						resultcache.put(Integer.parseInt(idcontent[0]), msg);
-						Connections.sendMsgToClient(msg, DpReceive);
 
-						break;
-					case 4:
-						// withdraw account
-						a = checkAccount(accList, message[2]);
-						if (a == null) {
-							msg = "4" + Constants.ACCOUNTNOTFOUND;
-						} else if (!a.getName().equals(message[1])) {
-							msg = "4" + Constants.INCORRECTNAME;
-						} else if (!a.getPassword().equals(message[3])) {
-							msg = "4" + Constants.INCORRECTPASSWORD;
+				if (DpReceive != null) {
+					String decodedMsg = Marshal.byteToString(receive);
+					String[] idcontent = Marshal.decodeForServer(decodedMsg);
+					String[] message = Marshal.decodeMessage(idcontent[1]);
+					String msg = "";
+					Account a = null;
+					if (msgCache != null) {
+						if (msgCache.containsKey(Integer.parseInt(idcontent[0]))) {
+							System.out.println("Duplicated Message ID: " + idcontent[0] + " is Received!");
+							String result = resultcache.get(Integer.parseInt(idcontent[0]));
+							msg = result;
+							System.out.println("Sending Following Message to Client: " + msg);
+							Connections.sendMsgToClient(msg, DpReceive);
+							continue;
 						} else {
-							if (a.getBalance() < Double.parseDouble(message[5])) {
-								msg = "4" + Constants.INSUFFICIENTBAL;
+							msgCache.put(Integer.parseInt(idcontent[0]), idcontent[1]);
+							System.out.println("I Have inputted id content 0 " + idcontent[0] + " and idcontent 1 "
+									+ idcontent[1]);
+						}
+
+					}
+
+					switch (Integer.parseInt(message[0])) {
+						case 0:
+							break;
+						case 1:
+							// create account
+							a = CreateAccount(accountnum, message[1], message[2], CURRENCY.valueOf(message[3]));
+							accList.add(a);
+							accountnum++;
+							msg = "1|SUCCESS|" + "Account successfully created, your account number is "
+									+ Marshal.accPadding(a.getNumber());
+							resultcache.put(Integer.parseInt(idcontent[0]), msg);
+							Connections.sendMsgToClient(msg, DpReceive);
+							break;
+						case 2:
+							// close account
+							a = checkAccount(accList, message[2]);
+							if (a == null) {
+								msg = "2" + Constants.ACCOUNTNOTFOUND;
+							} else if (!a.getName().equals(message[1])) {
+								msg = "2" + Constants.INCORRECTNAME;
+							} else if (!a.getPassword().equals(message[3])) {
+								msg = "2" + Constants.INCORRECTPASSWORD;
+							} else {
+								if (a.getBalance() >= 1) {
+									msg = "2|ERROR|"
+											+ "Account still have balance left! Please withdraw all amount before closing your account"
+											+ "|";
+								} else {
+									accList.remove(a);
+									msg = "2|SUCCESS|" + Marshal.accPadding(a.getNumber()) + " has been closed!|";
+								}
+							}
+							resultcache.put(Integer.parseInt(idcontent[0]), msg);
+							Connections.sendMsgToClient(msg, DpReceive);
+							break;
+						case 3:
+							// deposit account
+							a = checkAccount(accList, message[2]);
+							if (a == null) {
+								msg = "3" + Constants.ACCOUNTNOTFOUND;
+							} else if (!a.getName().equals(message[1])) {
+								msg = "3" + Constants.INCORRECTNAME;
+							} else if (!a.getPassword().equals(message[3])) {
+								msg = "3" + Constants.INCORRECTPASSWORD;
+							} else {
+								// TODO currency conversion
+								int i = accList.indexOf(a);
+								double bal = accList.get(i).getBalance();
+								accList.get(i).setBalance(bal + Double.parseDouble(message[5]));
+								msg = "3|SUCCESS|" + a.getNumber() + " balance is " + accList.get(i).getBalance() + "|";
+							}
+							resultcache.put(Integer.parseInt(idcontent[0]), msg);
+							Connections.sendMsgToClient(msg, DpReceive);
+
+							break;
+						case 4:
+							// withdraw account
+							a = checkAccount(accList, message[2]);
+							if (a == null) {
+								msg = "4" + Constants.ACCOUNTNOTFOUND;
+							} else if (!a.getName().equals(message[1])) {
+								msg = "4" + Constants.INCORRECTNAME;
+							} else if (!a.getPassword().equals(message[3])) {
+								msg = "4" + Constants.INCORRECTPASSWORD;
+							} else {
+								if (a.getBalance() < Double.parseDouble(message[5])) {
+									msg = "4" + Constants.INSUFFICIENTBAL;
+								} else {
+									int i = accList.indexOf(a);
+									double bal = accList.get(i).getBalance();
+									accList.get(i).setBalance(bal - Double.parseDouble(message[5]));
+									msg = "4|SUCCESS|" + "your account " + a.getNumber() + ": balance is "
+											+ accList.get(i).getBalance() + "|";
+								}
+							}
+							resultcache.put(Integer.parseInt(idcontent[0]), msg);
+							Connections.sendMsgToClient(msg, DpReceive);
+							break;
+						case 5:
+							// transfer money
+							Account owner = checkAccount(accList, message[2]);
+							Account receiver = checkAccount(accList, message[5]);
+							if (owner == null) {
+								msg = "5" + Constants.ACCOUNTNOTFOUND;
+							} else if (!owner.getName().equals(message[1])) {
+								msg = "5" + Constants.INCORRECTNAME;
+							} else if (!owner.getPassword().equals(message[3])) {
+								msg = "5" + Constants.INCORRECTPASSWORD;
+							} else {
+								if (receiver == null) {
+									msg = "5" + "|ERROR|" + "Transfer recipient account not found!|";
+								} else {
+									// update owner account
+									int i = accList.indexOf(owner);
+									double bal = accList.get(i).getBalance();
+									int j = accList.indexOf(receiver);
+									if (bal >= Double.parseDouble(message[6])) {
+										accList.get(i).setBalance(bal - Double.parseDouble(message[6]));
+										// update receiver account
+										bal = accList.get(j).getBalance();
+										accList.get(j).setBalance(bal + Double.parseDouble(message[6]));
+										msg = "6|SUCCESS|Transfer Successful! Your balance is "
+												+ accList.get(i).getBalance() + "|";
+									} else {
+										msg = "5" + "|ERROR|" + "Not enough Balance for transfer!|";
+									}
+
+								}
+							}
+							resultcache.put(Integer.parseInt(idcontent[0]), msg);
+							Connections.sendMsgToClient(msg, DpReceive);
+
+							break;
+						case 6:
+							// check balance
+							a = checkAccount(accList, message[2]);
+							if (a == null) {
+								msg = "6" + Constants.ACCOUNTNOTFOUND;
+							} else if (!a.getName().equals(message[1])) {
+								msg = "6" + Constants.INCORRECTNAME;
+							} else if (!a.getPassword().equals(message[3])) {
+								msg = "6" + Constants.INCORRECTPASSWORD;
 							} else {
 								int i = accList.indexOf(a);
 								double bal = accList.get(i).getBalance();
-								accList.get(i).setBalance(bal - Double.parseDouble(message[5]));
-								msg = "4|SUCCESS|" + "your account " + a.getNumber() + ": balance is "
+								msg = "2|SUCCESS|" + "Your account " + a.getNumber() + ": balance is "
 										+ accList.get(i).getBalance() + "|";
 							}
-						}
-						resultcache.put(Integer.parseInt(idcontent[0]), msg);
-						Connections.sendMsgToClient(msg, DpReceive);
-						break;
-					case 5:
-						// transfer money
-						Account owner = checkAccount(accList, message[2]);
-						Account receiver = checkAccount(accList, message[5]);
-						if (owner == null) {
-							msg = "5" + Constants.ACCOUNTNOTFOUND;
-						} else if (!owner.getName().equals(message[1])) {
-							msg = "5" + Constants.INCORRECTNAME;
-						} else if (!owner.getPassword().equals(message[3])) {
-							msg = "5" + Constants.INCORRECTPASSWORD;
-						} else {
-							if (receiver == null) {
-								msg = "5" + "|ERROR|" + "Transfer recipient account not found!|";
-							} else {
-								// update owner account
-								int i = accList.indexOf(owner);
-								double bal = accList.get(i).getBalance();
-								int j = accList.indexOf(receiver);
-								if (bal >= Double.parseDouble(message[6])) {
-									accList.get(i).setBalance(bal - Double.parseDouble(message[6]));
-									// update receiver account
-									bal = accList.get(j).getBalance();
-									accList.get(j).setBalance(bal + Double.parseDouble(message[6]));
-									msg = "6|SUCCESS|Transfer Successful! Your balance is "
-											+ accList.get(i).getBalance() + "|";
-								} else {
-									msg = "5" + "|ERROR|" + "Not enough Balance for transfer!|";
+							Connections.sendMsgToClient(msg, DpReceive);
+							break;
+						case 7:
+							// add broadcast
+							broadcastList.add(DpReceive);
+
+							break;
+						case 8:
+							for (DatagramPacket datagramPacket : broadcastList) {
+								if (datagramPacket.getAddress() == DpReceive.getAddress()) {
+									broadcastList.remove(datagramPacket);
 								}
-
 							}
-						}
-						resultcache.put(Integer.parseInt(idcontent[0]), msg);
-						Connections.sendMsgToClient(msg, DpReceive);
+							break;
 
-						break;
-					case 6:
-						// check balance
-						a = checkAccount(accList, message[2]);
-						if (a == null) {
-							msg = "6" + Constants.ACCOUNTNOTFOUND;
-						} else if (!a.getName().equals(message[1])) {
-							msg = "6" + Constants.INCORRECTNAME;
-						} else if (!a.getPassword().equals(message[3])) {
-							msg = "6" + Constants.INCORRECTPASSWORD;
-						} else {
-							int i = accList.indexOf(a);
-							double bal = accList.get(i).getBalance();
-							msg = "2|SUCCESS|" + "Your account " + a.getNumber() + ": balance is "
-									+ accList.get(i).getBalance() + "|";
-						}
-						Connections.sendMsgToClient(msg, DpReceive);
-						break;
-					case 7:
-						// add broadcast
-						broadcastList.add(DpReceive);
-
-						break;
-					case 8:
-						for (DatagramPacket datagramPacket : broadcastList) {
-							if (datagramPacket.getAddress() == DpReceive.getAddress()) {
-								broadcastList.remove(datagramPacket);
-							}
-						}
-						break;
-
-					default:
-						System.out.println("invalid input");
-						msg = "invalid";
-						break;
-				}
-				// Exit the server if the client sends "bye"
-				if (Marshal.byteToString(receive).equals("bye")) {
-					System.out.println("Client sent bye.....EXITING");
-					break;
-				}
-
-				if (!broadcastList.isEmpty()) {
-					for (int i = 0; i < broadcastList.size(); i++) {
-						// System.out.println("i" + i);
-						// System.out.println("broadcastList.get(i)" + broadcastList.get(i));
-						Connections.sendMsgToClient(msg, broadcastList.get(i));
+						default:
+							System.out.println("invalid input");
+							msg = "invalid";
+							break;
 					}
-				}
+					// Exit the server if the client sends "bye"
+					if (Marshal.byteToString(receive).equals("bye")) {
+						System.out.println("Client sent bye.....EXITING");
+						break;
+					}
 
-				// Clear the buffer after every message.
-				receive = new byte[65535];
+					if (!broadcastList.isEmpty()) {
+						for (int i = 0; i < broadcastList.size(); i++) {
+							// System.out.println("i" + i);
+							// System.out.println("broadcastList.get(i)" + broadcastList.get(i));
+							Connections.sendMsgToClient(msg, broadcastList.get(i));
+						}
+					}
+
+					// Clear the buffer after every message.
+					receive = new byte[65535];
+				}
 			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -296,7 +301,7 @@ public class Server {
 		return ret;
 	}
 
-	public static int getRand(){
+	public static int getRand() {
 		Random r = new Random();
 		return r.nextInt(10);
 	}
